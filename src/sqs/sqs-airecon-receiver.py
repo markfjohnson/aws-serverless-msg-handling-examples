6f91b826dc6bb8cbc47c33e9afdaef8439cf5422
Mark Johnson <markfjohnson@gmail.com>
2023-01-02 20:51:49 -0500

    Setup Initial SQS and SNS example

--- src/sqs/sqs-airecon-receiver.py
+++ src/sqs/sqs-airecon-receiver.py
@@ -1,18 +1,26 @@
 import json
 import urllib3
 import os
-
+import boto3
 
 def lambda_handler(event, context):
     try:
+        queueUrl = os.environ['QUEUE_URL']
+        sqs_client = boto3.client('sqs')
         print(f"This is what API Gateway passed to the Lambda receiver ")
         body = event.get('body')
         print(body)
         i = 0
         i = i + 1
 
+        response = sqs_client.send_message(
+            QueueUrl=queueUrl,
+            MessageBody= body
+
+        )
+        print(f"Sent message")
         return {
-            'statusCode': 200,
+            'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
             "isBase64Encoded": False,
             'headers': {
                 'Content-Type': 'application/json'
